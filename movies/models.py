b6d164df411136f6dd50a91f71a17a6e5782f7bd
Emmanuel Cangy <ecangy0102@gmail.com>
2022-12-06 23:10:14 +0400

    linked html and css to the index.html

--- movies/models.py
+++ movies/models.py
@@ -7,7 +7,6 @@ class Choice(models.Model):
     def __str__(self):
         return self.provider
 
-
 class Question(models.Model):
     movie_name = models.CharField(max_length=200)
     movie_description = models.CharField(max_length=300, default='')
