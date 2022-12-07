f4357118cbd0c247fd7d37a7a7ba180bc1adcd79
Emmanuel Cangy <ecangy0102@gmail.com>
2022-12-07 11:36:30 +0400

    fixed useless parameters and changed movie.html

--- movies/views.py
+++ movies/views.py
@@ -5,15 +5,24 @@ from django.template import loader
 # Create your views here.
 def index(request):
     latest_movie_list = Watched.objects.all()
+    all_choice_list = Choice.objects.all()
     context = {
         'latest_movie_list': latest_movie_list,
+        'all_choice_list': all_choice_list,
     }
     return render(request, 'movies/index.html', context)
 
 
-def movie(request, watched_id=1):
+def movie(request, watched_id):
     movie = Watched.objects.get(id=watched_id)
+    all_choice_list = Choice.objects.all()
+    # for provider in providers:
+    #     if (movie.provider222() == provider_id):
+    #         specificProvider = provider.id
+
     context = {
         'movie': movie,
+        'all_choice_list': all_choice_list,
+
     }
     return render(request, "movies/movie.html", context)
